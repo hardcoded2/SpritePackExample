dc951a16e44b6f282ae048ab6b155f96bec53862
Alex Sink <alexsink@yahoo.com>
2020-03-17 21:24:22 -0700

    basic 8x8 example for sprites

---
+++ Assets/Scripts/SimpleAtlas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SimpleAtlas : MonoBehaviour
+{
+    //evenly packed assumed
+    [Serializable]
+    public class AtlasInfo
+    {
+        public Vector2 SpriteSizeInPixels = Vector2.one * 50f;
+        public Texture2D Tex; //duplicate info for now
+
+        public TextureSettings SettingsForImage(Vector2Int texNumber)
+        {
+            Assert.AreEqual(Tex.height, Tex.width); //square only supported
+            var upDownUnit = Tex.height / SpriteSizeInPixels.y; // 400/50 = 8
+            //1 / 8 (where 8 is number of things) = .125 - uvspace increment
+            var leftRightUnit = Tex.width / SpriteSizeInPixels.x;
+            var upDownUnitUV = 1/upDownUnit; // 1/8 = 0.125
+            var leftRightUnitUV = 1/leftRightUnit;
+            var maxVal = new Vector2Int(Mathf.RoundToInt(leftRightUnit), Mathf.RoundToInt(upDownUnit)); // should be 8,8
+            Assert.IsTrue(texNumber.x >= 0 && texNumber.x < maxVal.x);
+            Assert.IsTrue(texNumber.y >= 0 && texNumber.y < maxVal.y);
+
+            return new TextureSettings
+            {
+                Tiling = new Vector2(upDownUnitUV, leftRightUnitUV),
+                Offset = new Vector2(leftRightUnitUV * texNumber.x, 1- (upDownUnitUV * (texNumber.y+1)))
+            };
+        }
+    }
+
+    public class TextureSettings
+    {
+        public Vector2 Offset;
+        public Vector2 Tiling;
+    }
+
+    [SerializeField] private AtlasInfo m_AtlasInfo;
+}
---
+++ Assets/Scripts/Tests/EditorTest/Edit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Edit
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void EditSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            var atlasInfo = new SimpleAtlas.AtlasInfo()
+            {
+                Tex = new Texture2D(400,400),SpriteSizeInPixels = 50f * Vector2.one
+            };
+            //Assert.AreApproximatelyEqual(atlasInfo.Tex.width,50f);
+            //Assert.AreApproximatelyEqual(atlasInfo.Tex.height,50f);
+            var settingsForImage = atlasInfo.SettingsForImage(new Vector2Int(0, 0));
+            Assert.AreEqual(new Vector2(0.125f,0.125f ),settingsForImage.Tiling);
+            Assert.AreEqual(new Vector2(0,0.875f), settingsForImage.Offset);
+                
+            settingsForImage = atlasInfo.SettingsForImage(new Vector2Int(1, 0));
+            Assert.AreEqual(new Vector2(0.125f,0.125f ),settingsForImage.Tiling);
+            Assert.AreEqual(new Vector2(0.125f,0.875f), settingsForImage.Offset);
+            
+            settingsForImage = atlasInfo.SettingsForImage(new Vector2Int(0, 1));
+            Assert.AreEqual(new Vector2(0.125f,0.125f ),settingsForImage.Tiling);
+            Assert.AreEqual(new Vector2(0f,0.75f), settingsForImage.Offset);
+            
+            settingsForImage = atlasInfo.SettingsForImage(new Vector2Int(0, 7));
+            Assert.AreEqual(new Vector2(0f,0f), settingsForImage.Offset);
+            
+            Assert.AreEqual(new Vector2(0.875f,0f), atlasInfo.SettingsForImage(new Vector2Int(7, 7)).Offset);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator EditWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
